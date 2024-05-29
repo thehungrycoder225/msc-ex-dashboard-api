233503ef9aa55c0dee8b1c0b4a459b8ca0d7bdd0
Dane <golightning225@gmail.com>
2024-05-29 14:14:19 +0800

    minor modifications

--- functions/api.js
+++ functions/api.js
@@ -3,23 +3,35 @@ const { dbConnect } = require('./config/db');
 const resolvers = require('./graphql/resolvers');
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+const { expressMiddleware } = require('@apollo/server/express4');
 const logger = require('./utils/logger');
 const dotenv = require('dotenv');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-app.use(cors());
-app.use('/.netlify/functions/api/graphql');
+const http = require('http');
 
 dotenv.config();
 dbConnect();
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const { url } = startStandaloneServer(server, {
-  context: async ({ req }) => ({ token: req.headers.token }),
-  listen: { port: 4000 },
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-console.log(`🚀  Server ready at ${url}`);
+const handleServerStart = async () => {
+  await server.start();
+
+  app.use('/graphql', express.json(), expressMiddleware(server));
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+};
+handleServerStart();
+app.use(cors());
 // server.use('/.netlify/functions/api/graphql');
 module.exports.handler = serverless(app);
